bf53900f040cb4efb4331c5666160cc312969b64
kevin <k-hr123@hotmail.com>
2024-11-24 15:33:47 -0500

    actualizacion de codigo django, creaci creacion de dos tablas y sus respectivas migraciones y añadir depencia ipython

--- my_first_project/my_first_app/models.py
+++ my_first_project/my_first_app/models.py
@@ -10,3 +10,22 @@ class Car(models.Model):   # hereda los modelos de .models
 
     def __str__(self):             # esta clase muestra la representacion textual de lo que se quiere ver dentro de 
         return f"{self.title} - {self.year} - {self.color}"         # la terminal de un objeto especifico
+    
+class Publisher(models.Model):    # para observar las relaciones de tablas con django creamos como ejemplo dos clases
+    name = models.TextField(max_length=200)   # una para los editores y una tabla para los libros
+    address = models.TextField(max_length=30)
+
+    def __str__(self) -> str:      # para poder ver en consola
+        return self.name
+    
+class Book(models.Model):                      # clase libros
+    title = models.TextField(max_length=200)   # titulo del libro 
+    publications_date = models.DateField()     # fecha de publicacion del libro
+    Publisher = models.ForeignKey(Publisher, on_delete=models.DO_NOTHING)   # se establece la relacion de las tablas, en los parametros se establece la
+                                               # la clase a relacionar en este caso con publisher
+        # on_delete se utiliza para tomar una decision en caso de que se borre un publisher(editor)
+        # DO_NOTHING  no hace nada, PROTECT no se puede borrar porque aun hay datos con ese publisher
+        # CASCADE si se borra un publisher tambien borra todos los libros relacionados con este
+    
+    def __str__(self) -> str:
+        return self.title             # mostramos el titulo en consola
